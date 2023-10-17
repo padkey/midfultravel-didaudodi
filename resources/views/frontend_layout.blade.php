1936c509e59dbe3d160b7dd007e53f3e2ff4c2c8
nguyenxuanly <william.lynguyen@gmail.com>
2023-10-17 11:30:21 +0700

    fix home pages shdfjsh

--- resources/views/frontend_layout.blade.php
+++ resources/views/frontend_layout.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- <link rel="manifest" href="site.webmanifest"> -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{'uploads/'.$logoWhite->items[0]->path_desktop}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{url('frontend/images/s-logo.png')}}">
     <!-- Place favicon.ico in the root directory -->
 
     <!-- CSS here -->
